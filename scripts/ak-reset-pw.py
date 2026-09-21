064aa413126e6d4791fb1ceb3ac554857baac53f
cyberscoundrel <cyberscoundrel@gmail.com>
2026-09-21 17:27:02 -0500

    akadmin reset script (django settings env)

--- scripts/ak-reset-pw.py
+++ scripts/ak-reset-pw.py
@@ -9,8 +9,8 @@ django.setup()
 from django.contrib.auth import get_user_model
 
 user_model = get_user_model()
-new_password = os.environ.get("AKADMIN_NEW_PASSWORD", "reset-pass-909")
+new_password = "reset-akadmin-909"
 u = user_model.objects.get(username="akadmin")
 u.set_password(new_password)
 u.save()
-print("akadmin password reset OK")
+print("akadmin password reset OK ->", new_password)
